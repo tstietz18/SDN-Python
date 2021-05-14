f7da775d0f3063c2d564cb9d807441d9398c6089
Tory Stietz <83377829+tstietz18@users.noreply.github.com>
2021-05-14 10:16:51 -0500

    Update NXAPI1.1.py

--- NXAPI1.1.py
+++ NXAPI1.1.py
@@ -13,8 +13,42 @@
 #
 #****************************
 
+
+
 import requests
 import json
+
+"""
+Modify these please
+"""
+switchuser='USERID'
+switchpassword='PASSWORD'
+
+url='http://10.10.20.177/ins'
+myheaders={'content-type':'application/json-rpc'}
+payload=[
+  {
+    "jsonrpc": "2.0",
+    "method": "cli",
+    "params": {
+      "cmd": "configure terminal",
+      "version": 1
+    },
+    "id": 1
+  },
+  {
+    "jsonrpc": "2.0",
+    "method": "cli",
+    "params": {
+      "cmd": "hostname dist-sw01-9k",
+      "version": 1
+    },
+    "id": 2
+  }
+]
+response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+
+
 
 #function to Validate a Hostname
 def isvalidhostname(HostName):
