f2c1e59b660cfa1e173aeda29a83f7a94f58c93a
Tory Stietz <83377829+tstietz18@users.noreply.github.com>
2021-05-16 12:50:24 -0500

    Add files via upload

--- NXAPI1.1.py
+++ NXAPI1.1.py
@@ -5,7 +5,7 @@
 # 
 #Date created: 5/5/2021
 # 
-#Script Function: 
+#Script Function: Ask for a hostname
 # 
 #Script References:
 # 
@@ -13,41 +13,53 @@
 #
 #****************************
 
-
-
 import requests
 import json
 
-"""
-Modify these please
-"""
-switchuser='cisco'
-switchpassword='cisco'
+def ChangeHostName()
+   
+    """
+    Modify these please
+    """
+    switchuser='cisco'
+    switchpassword='cisco'
 
-url='http://10.10.20.177/ins'
-myheaders={'content-type':'application/json-rpc'}
-payload=[
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "configure terminal",
-      "version": 1
+    url='http://10.10.20.177/ins'
+    myheaders={'content-type':'application/json-rpc'}
+    payload=[
+    {
+        "jsonrpc": "2.0",
+        "method": "cli",
+        "params": {
+        "cmd": "configure terminal",
+        "version": 1
+        },
+        "id": 1
     },
-    "id": 1
-  },
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "hostname dist-sw01-9k",
-      "version": 1
-    },
-    "id": 2
-  }
-]
-response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+    {
+        "jsonrpc": "2.0",
+        "method": "cli",
+        "params": {
+        "cmd": "hostname dist-sw01-9k",
+        "version": 1
+        },
+        "id": 2
+    }
+    ]
 
+    '''
+
+    verify=False below is to accept untrusted certificate
+
+    '''
+
+    response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+
+    return response
+    
+#Main Code
+
+responseHostName = 
 
 
 #function to Validate a Hostname
@@ -78,93 +90,6 @@ while validHost == False:
     validHost = isvalidhostname(HostName)
     if validHost == False:
         print("Enter a Valid Hostname")
-getNewHostName(HostName)
-
-#Asks the user to enter an Ip address and splits it into four integers.
-IpAddress= input('Enter a new IP Address:')
-print IpAddress
-
-ipList =  IpAddress.split(".")
-
-#Adds two to the new Ip Address and prints the Address.
-def AddToIP(ipList):
-
-    octet3 = int(myList[2])
-    octet3 = octet3 + 2
-    myList[2] = str(octet3)
-
-    newIP = myList [0] + "." + myList[1] + "." + myList[2] + "." + myList[3]
-
-    print("newIP")
-
-#Validates the new Ip Address.
-def ValidateIp(newIP):
-    
-    if len(ipList) == 4:
-        validEntry = True
-    else:
-        validEntry = False
-    if validEntry == True:
-        for Octet in ipList:
-            if Octet.isdigit() == False:
-                validEntry = False
-    if validEntry == True:
-        for Octet in ipList:
-            if int(Octet) < 0 or int(Octet) > 255:
-                validEntry = False
-    if validEntry == True :
-        print("That is a Valid IP")
-    else:
-         print("Enter a Valid Ip")
-
-def GetNewIpAddress(IpAddress + SubnetMask)
-
-    """
-    Be sure to run feature nxapi first on Nexus Switch
-
-    """
-    #sets the username and password to the values in the single quotations.
-    switchuser='cisco'
-    switchpassword='cisco'
-
-    url='https://10.10.20.177/ins'
-    myheaders={'content-type':'application/json-rpc{
-            "jsonrpc": "2.0",
-            "method": "cli",
-            "params": {
-              "cmd": "configure terminal",
-              "version": 1
-            },
-            "id": 1
-          },
-          {
-            "jsonrpc": "2.0",
-            "method": "cli",
-            "params": {
-              "cmd": "ip address " + IpAddress + SubnetMask,
-              "version": 1
-             },
-            "id": 2
-          }
-        ]
-      
-    '''
-
-    verify=False below is to accept untrusted certificate
-
-    '''
+getisvalidhostname(HostName)
 
 
-
-##"10.1.1.254"
-##
-##['10','1','1','254']
-##
-##
-##octet3 = int(myList[2])
-##octet3 = octet3 + 2
-##myList[2] = str(octet3)
-##
-##newIP = myList [0] + "." + myList[1] + "."  etc..
-
-# end of script
